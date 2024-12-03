d1e6c75195010da63e51a36951f3cd767a32bb0b
cngonz <149430790+cngonz@users.noreply.github.com>
2024-12-03 14:52:18 -0500

    Update data.py
    Overhauled the original system I was using this one allows for a clean display of information and more specific similarity search

--- data.py
+++ data.py
@@ -11,44 +11,88 @@ sauthcode = rauthcode.text
 authcode = sauthcode[17:47]
 wrapper = IGDBWrapper("h730xa66skz2uolwot80wok0ghjlt4", authcode)
 
+# Transforms json data into a game object making it easier to read
+def easy_read(item):
+    id = item.get("id")
+    name = item.get("name")
+    tempgenres = item.get("genres", [])
+    genres = []
+    for g in tempgenres:
+        genres.append(g.get("name"))
+    tempthemes = item.get("themes", [])
+    themes = []
+    for t in tempthemes:
+        themes.append(t.get("name"))
+    game = Game(id, name, genres, themes)
+    return game
 
+# Game class
 class Game:
-    def __init__(self, name):
-        self.name = "\"" + name + "\""
-    def get_id(self):
-        r = wrapper.api_request("games", f"where name = {self.name};")
-        raw_data = r.decode("utf-8")
-        data = codecs.decode(raw_data, "unicode_escape")
-        endIndex = data.find(" ", 16)
-        return data[16:endIndex-1]
-    def get_genre(self):
-        r = wrapper.api_request("games", f"fields genres.name; where name = {self.name};")
-        raw_data = r.decode("utf-8")
-        data = codecs.decode(raw_data, "unicode_escape")
-        startIndex = data.find("id",20)+5
-        endIndex = data.find(" ", startIndex+1)
-        return data[startIndex:endIndex-2]
-    def get_same_genre_games(self,limit):
-        r = wrapper.api_request("games", f"fields name; where genres = {int(self.get_genre())}; limit {limit};")
-        raw_data = r.decode("utf-8")
-        data = codecs.decode(raw_data, "unicode_escape")
-        nameList = []
-        i = 0
-        while (i != -1):
-            i = data.find("name", i)
-            if i == -1:
-                break
-            startIndex = i + 8
-            i = data.find("\"", startIndex+1)
-            if i == -1:
-                break
-            endIndex = i
-            nameList.append(data[startIndex:endIndex])
-            i = endIndex + 1
-        return nameList
+    def __init__(self, id, name, genres, themes):
+        self.name = name
+        self.id = id
+        self.genres = genres
+        self.themes = themes
+    def __str__(self):
+        sgenres = ""
+        for g in self.genres:
+            sgenres = sgenres + g + " "
+        sthemes = ""
+        for t in self.themes:
+            sthemes = sthemes + t + " "
+        return(
+            f"Game ID: {self.id}\n"
+            f"Name: {self.name}\n"
+            f"Genres: {sgenres}\n"
+            f"Themes: {sthemes}\n"
+        )
+    def get_theme_id(self):
+        themeID = []
+        for t in self.themes:
+            r = wrapper.api_request("themes", f"fields id, name; where name = \"{t}\";")
+            raw_data = r.decode("utf-8")
+            jdata = json.loads(raw_data)
+            for item in jdata:
+                tid = item.get("id")
+                themeID.append(tid)
+        return themeID
+    def get_genre_id(self):
+        genreID = []
+        for g in self.genres:
+            r = wrapper.api_request("genres", f"fields id, name; where name = \"{g}\";")
+            raw_data = r.decode("utf-8")
+            jdata = json.loads(raw_data)
+            for item in jdata:
+                gid = item.get("id")
+                genreID.append(gid)
+        return genreID
 
-obj = Game("Arthur's Camping Adventure")
-print(obj.get_id())
-print(obj.get_genre())
-print(obj.get_same_genre_games(3))
+    def get_similar_games(self,limit):
+        genre_list = self.get_genre_id()
+        theme_list = self.get_theme_id()
+        gl = ""
+        tl = ""
+        for g in genre_list:
+            gl = gl + str(g) + ", "
+        for t in theme_list:
+            tl = tl + str(t) + ", "
+        gl = gl[0:(len(gl)-2)]
+        tl = tl[0:(len(tl)-2)]
+        r = wrapper.api_request("games", f"fields name, genres.name, themes.name; where genres = ({gl}) & themes = ({tl});  limit {limit};")
+        raw_data = r.decode("utf-8")
+        jdata = json.loads(raw_data)
+        return jdata
+
+
+# Main to test
+if __name__=='__main__':
+    name = input("\nPlease enter name of a game: ")
+    print("\n")
+    r = wrapper.api_request("games", f"fields name, genres.name, themes.name; where name = \"{name}\";  limit {1};")
+    raw_data = r.decode("utf-8")
+    jdata = json.loads(raw_data)[0]
+    game = easy_read(jdata)
+    data = game.get_similar_games(10)
+    for g in data:
+        print(easy_read(g))
 
