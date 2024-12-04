ef6aa909ef5801f96044fd1b8b05aa12a954d4b6
TidalGator <117106208+TidalGator@users.noreply.github.com>
2024-12-04 17:23:08 -0500

    Update GeneralistFunctions.py
    commented out code that calls functions that does not exist

--- GeneralistFunctions.py
+++ GeneralistFunctions.py
@@ -7,9 +7,7 @@ import GameSet
 #Cesar did me a favor and went ahead and made the node class.
 #Each node contains the following: a name data aspect, and the genres the game has.
 #Additionally, each game has the ability to reach out and pull games of a similar genre.
-#Thus, I can use this ability to help create a minimum spanning tree of games similar to the source game.
-#Ergo, It would probably be more useful for me to make the algorithms which will first create a tree of all games similar to this game, and THEN generate a MST from that tree.
-
+#Thus, I can use this ability to help create a list of games similar to the source game.
 
 def makeSourceVertex(name):
     return data.get_game(name) #The game object the user searched for
