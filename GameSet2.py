072b84d7c3cce90e71558a8429b1f756b10d0e53
mkuzminskiy8 <mkuzminskiy@ufl.edu>
2024-12-04 05:34:13 -0500

    small update

--- GameSet2.py
+++ GameSet2.py
@@ -10,7 +10,7 @@ class Node(Game):
 class LinkedList(Game):
     def __init__(self, Game):
         self.head = None
-        self.score = 1
+        self.score = Game.similarity_score()
 
     #Code borrowed from https://www.datacamp.com/tutorial/python-linked-lists mostly as a refresher on how Python works
     def insert_end(self, Game):
@@ -28,10 +28,12 @@ class LinkedList(Game):
 
 # Hash Table object that will be used to hold Linked Lists of games based on the similarity score
 class HashTable(LinkedList):
-    def __init__(self, LinkedList):
+    def __init__(self):
         self.Table = []
-        self.LinkedList = LinkedList
 
     def hash_function(self, LinkedList):
         scorehash = LinkedList.get_score()
+
+    def insert_list(self, LinkedList):
+        self.Table[LinkedList.get_score()] = LinkedList
 
