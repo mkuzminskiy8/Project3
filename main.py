25cb500cc976c5c99791caad869050ba41329414
cngonz <149430790+cngonz@users.noreply.github.com>
2024-12-04 18:35:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ if __name__=='__main__':
         if GameToSelect == "DoneForNow":
             break
         else:
-            Choice = input("How do we want to do this? you may type either \"heap\" or \"set\": ")
-            TimeStart = time.time()
+            Choice = input("How do we want to do this? you may type either \"heap\" or \"hash table\": ")
+            TimeStart = time.perf_counter()
             #Insert code here
             if Choice == "heap":
-                GeneralistFunctions.WorkWithHeap(GameToSelect)
-            elif Choice == "set":
-                GeneralistFunctions.WorkWithSet(GameToSelect)
+                if GeneralistFunctions.WorkWithHeap(GameToSelect) != None:
+                    TimeEnd = time.perf_counter()
+                    print(f"That took {round(TimeEnd - TimeStart, 2)} long with your decision to go with {Choice}")
+            elif Choice == "hash table":
+                if GeneralistFunctions.WorkWithSet(GameToSelect) != None:
+                    TimeEnd = time.perf_counter()
+                    print(f"That took {round(TimeEnd - TimeStart, 2)} long with your decision to go with {Choice}")
 
-            TimeEnd = time.time()
 
-            print("That took " + (TimeEnd - TimeStart) + " long with your decision to go with " + Choice) 
-            pass #when ready, make this game object by searching for the game in the data base. 
+
+
